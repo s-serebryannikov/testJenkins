6f2c2f1decbb9001c10babbc9dae2d950beb3dbe
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2023-03-19 22:16:28 +0300

    first commit

---
+++ src/main/java/ru/serebryannikovvv/TestServlet.java
@@ -0,0 +1,30 @@
+package ru.serebryannikovvv;
+
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/hello")
+public class TestServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Jenkins";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf8");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+        out.close();
+    }
+
+    public void destroy() {
+    }
+}
+
